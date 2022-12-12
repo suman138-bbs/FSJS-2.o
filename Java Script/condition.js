49484ed9998b6e529ffbde6a066709c026beb44e
suman138-bbs <sumanbhandari9860@gmail.com>
2022-12-12 17:23:23 +0545

    Added

---
+++ Java Script/condition.js
@@ -0,0 +1,33 @@
+// Rating mini
+
+// let rating = 5;
+
+// if(rating==5){
+//     console.log("5 star")
+// }
+// if(rating==4){
+//     console.log("4 star")
+// }
+// if(rating==3){
+//     console.log("3 star")
+// }
+/*Math */
+// console.log(Math.round(9.8))
+// console.log(Math.floor(9.8))
+// console.log(Math.ceil(9.4))
+
+// Min Max
+
+
+// console.log(Math.min(12, 3, 4, 5, 566, 1))
+// console.log(Math.round(Math.random()*10))
+
+
+/*Loops*/
+
+// objects
+let student = {
+    name:"Suman",rollNo:"190138010036",course:"B-Tech"
+}
+
+console.log(student.name)
--- Java Script/new.js
+++ Java Script/new.js
@@ -1,4 +1,26 @@
 let n=524356363;
-while(1){
-console.log("My name is suman");
+// while(1){
+// console.log("My name is suman");
+// }
+
+
+// if (n == 0)
+// {
+//     console.log("True")
+// }
+// else {
+//     console.log("False")
+// }
+
+
+let rating = 5;
+
+if(rating==5){
+    console.log("5 star")
+}
+if(rating==4){
+    console.log("4 star")
+}
+if(rating==3){
+    console.log("3 star")
 }
