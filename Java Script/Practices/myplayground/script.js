87db26ce9a016a2b6d9a31f4bd6308592ff8e6e8
suman138-bbs <sumanbhandari9860@gmail.com>
2023-01-21 19:21:15 +0530

    newint

---
+++ Java Script/Practices/myplayground/script.js
@@ -0,0 +1,33 @@
+// let arr = ["suman"]
+// arr.push("Bhandari");
+// console.log(arr);
+
+// let obj = {
+//     1: function fun1() {
+//         console.log("You are inside a function");
+
+//     }
+//     , 2: "Bishal",
+
+    
+// }
+// console.log(obj[1].fun1)
+
+// Object Accessing
+
+let first = {
+    name:"Suman Bhandari",
+    age:"22"
+}
+
+let second = {
+    name: "Sagar Tiwari",
+    age2:"22"
+}
+
+function f1(obj)
+{
+    console.log(second.age2)
+}
+
+f1(second)
