dab1f9f8d39032443dded293b52e942a320e5473
suman138-bbs <sumanbhandari9860@gmail.com>
2023-02-24 22:26:53 +0530

    Js Practices

--- Java Script/Practices/23_Feb JS/script.js
+++ Java Script/Practices/23_Feb JS/script.js
@@ -18,4 +18,18 @@
 // console.log(f2)
 
 
-let arr = ['1','2','3']
+// let arr = ['1','2','3']
+
+// 
+console.log("Fetching Data")
+
+let a = fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => {
+        return res.json()
+    }).then((data) => {
+        console.log(data.value)
+    })
+
+console.log(a.then(resp => {
+        return resp.json()
+    }).then())
